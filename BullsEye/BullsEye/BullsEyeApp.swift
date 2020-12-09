504d8f937e9cd32096ebd801d5028456cf490398
Brian <bfvilchez@icloud.com>
2020-12-09 11:03:06 -0700

    Bullseye Project
    Created a new project called Bullseye

---
+++ BullsEye/BullsEye/BullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeApp.swift
+//  BullsEye
+//
+//  Created by Brian Vilchez on 12/9/20.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
