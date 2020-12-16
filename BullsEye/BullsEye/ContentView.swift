64425876ba1c15f469fa8ce17ec0fb6078cb59ba
Brian <bfvilchez@icloud.com>
2020-12-15 23:33:09 -0700

    ContentView
    Added a slider, text and button to view.

--- BullsEye/BullsEye/ContentView.swift
+++ BullsEye/BullsEye/ContentView.swift
@@ -9,8 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("🎯🎯🎯 \nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .multilineTextAlignment(.center)
+                .padding(.horizontal)
+            Text(/*@START_MENU_TOKEN@*/"89"/*@END_MENU_TOKEN@*/)
+            HStack {
+                Text(/*@START_MENU_TOKEN@*/"1"/*@END_MENU_TOKEN@*/)
+                    .padding(.leading)
+                VStack {
+                    Slider(value: .constant(50), in: 1.0...100.0)
+                    Button(action: {}) {
+                        Text("HIT ME")
+                    }
+                }
+                Text(/*@START_MENU_TOKEN@*/"100"/*@END_MENU_TOKEN@*/)
+                    .padding(.trailing)
+            }
+                
+            
+        }
     }
 }
 
