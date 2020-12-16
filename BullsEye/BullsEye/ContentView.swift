98672aa07e526eb2a971c593a2e18611a0360c56
Brian <bfvilchez@icloud.com>
2020-12-16 11:31:41 -0700

    ContentView
    Added a popup alert.

--- BullsEye/BullsEye/ContentView.swift
+++ BullsEye/BullsEye/ContentView.swift
@@ -8,30 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var alertIsVisible: Bool = false
+    @State private var sliderValue: Double = 50
+    
     var body: some View {
         VStack {
-            Text("🎯🎯🎯 \n PUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
-                .multilineTextAlignment(.center)
-                .padding(.horizontal)
-            Text("57")
-                .bold()
-                .font(Font.system(size: 30))
-            HStack {
-                Text(/*@START_MENU_TOKEN@*/"1"/*@END_MENU_TOKEN@*/)
-                    .padding(.leading)
-                VStack {
-                    Slider(value: .constant(50), in: 1.0...100.0)
-                    Button(action: {
-                        print("Hello there!")
-                    }) {
-                        Text("HIT ME")
+            VStack {
+                Text("🎯🎯🎯 \n PUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                    .bold()
+                    .kerning(2.0)
+                    .lineSpacing(4.0)
+                    .font(.footnote)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal)
+                Text("57")
+                    .bold()
+                    .font(Font.system(size: 30))
+                HStack {
+                    Text(/*@START_MENU_TOKEN@*/"1"/*@END_MENU_TOKEN@*/)
+                        .padding(.leading)
+                    VStack {
+                        Slider(value: self.$sliderValue, in: 1.0...100.0)
+                        Button(action: {
+                            self.alertIsVisible = true
+                            print("Hello there!")
+                        }) {
+                            Text("HIT ME")
+                        }
+                        .alert(isPresented: $alertIsVisible, content: {
+                            return Alert(title: Text("Hello there"), message: Text("The value is \(self.sliderValue)"), dismissButton: .default(Text("Done")))
+                        })
+                        
                     }
+                    Text(/*@START_MENU_TOKEN@*/"100"/*@END_MENU_TOKEN@*/)
+                        .padding(.trailing)
                 }
-                Text(/*@START_MENU_TOKEN@*/"100"/*@END_MENU_TOKEN@*/)
-                    .padding(.trailing)
-            }
+                    
                 
-            
+            }
         }
     }
 }
